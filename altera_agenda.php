86c314ffe1eaae49ee91e8cbdd24004449e42d73
Mário <mmm.masaomukai@gmail.com>
2023-06-19 17:25:33 -0300

    bootstrap

--- altera_agenda.php
+++ altera_agenda.php
@@ -11,36 +11,66 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=h1, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+
     <title>Document</title>
 </head>
 <body>
-    <h1>Alteração de Agendas - IFSP</h1>
-    <form action="altera_agenda_exe.php" method="POST" enctype="multipart/form-data">
+<header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container">
+            <a class="navbar-brand" href="#">Meu Site</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+              <li class="nav-item">
+                  <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="cadastro_agenda.html">Cadastrar</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="listar_agenda.php">Listar</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+    <h1 class="container">Alteração de Agendas - IFSP</h1>
+    <form action="altera_agenda_exe.php" method="POST" enctype="multipart/form-data" class="container my-5">
         <input type="hidden" name="id_agenda" value="<?php echo $row['id_agenda']?>">
-        <div>
+        <div class="row mt-3">
+        <div class="col">
             <label for="nome">Nome</label>
-            <input type="text" name="nome" id="nome" value="<?php echo $row['nome']?>">
+            <input type="text" name="nome" id="nome" value="<?php echo $row['nome']?>" class="form-control">
         </div>
-        <div>
+        <div class="col">
             <label for="apelido">Apelido</label>
-            <input type="text" name="apelido" id="apelido" value="<?php echo $row['apelido']?>">
+            <input type="text" name="apelido" id="apelido" value="<?php echo $row['apelido']?>" class="form-control">
         </div>
-        <div>
+        </div>
+        <div class="row mt-3">
+        <div class="col">
             <label for="endereco">Endereço</label>
-            <input type="text" name="endereco" id="endereco" value="<?php echo $row['endereco']?>">
+            <input type="text" name="endereco" id="endereco" value="<?php echo $row['endereco']?>" class="form-control">
         </div>
-        <div>
+        <div class="col-4">
             <label for="bairro">Bairro</label>
-            <input type="text" name="bairro" id="bairro" value="<?php echo $row['bairro']?>">
+            <input type="text" name="bairro" id="bairro" value="<?php echo $row['bairro']?>" class="form-control ">
         </div>
-        <div>
+        </div>
+        <div class="row mt-3">
+        <div class="col">
             <label for="cidade">Cidade</label>
-            <input type="text" name="cidade" id="cidade" value="<?php echo $row['cidade']?>">
+            <input type="text" name="cidade" id="cidade" value="<?php echo $row['cidade']?>" class="form-control">
         </div>
-        <div>
+        <div class="col-2">
             <label for="estado">Estado</label>
             <?php 
-                echo "<select name='estado' id='estado' size='1'>";
+                echo "<select name='estado' id='estado' size='1' class='form-select'>";
                 switch($row['estado']){
                     case "AC":
                         echo "<option  selected value='AC'>Acre</option>";
@@ -830,31 +860,52 @@
             ?>
             
         </div>
-        <div>
+        </div>
+        <div class="row mt-3">
+        <div class="col">
             <label for="fone">Telefone</label>
             <input type="tel" name="fone" id="fone" 
             placeholder="Formato (99) 9999-9999"
-            pattern="\([0-9]{2}\)([9]{1})?[0-9]{4}-[0-9]{4}" value="<?php echo $row['telefone']?>">
+            pattern="\([0-9]{2}\)([9]{1})?[0-9]{4}-[0-9]{4}" value="<?php echo $row['telefone']?>" class="form-control">
         </div>
-        <div>
+        <div class="col">
             <label for="celular">Celular</label>
             <input type="tel" name="celular" id="celular" 
             placeholder="Formato (99) 9999-9999"
-            pattern="\([0-9]{2}\)([9]{1})?[0-9]{4}-[0-9]{4}" value="<?php echo $row['celular']?>">
+            pattern="\([0-9]{2}\)([9]{1})?[0-9]{4}-[0-9]{4}" value="<?php echo $row['celular']?>" class="form-control">
         </div>
-        <div>
+        </div>
+        
+        <div class="row mt-3">
+        <div class="col">
             <label for="email">E-mail</label>
-            <input type="email" name="email" id="email" value="<?php echo $row['email']?>">
+            <input type="email" name="email" id="email" value="<?php echo $row['email']?>" class="form-control">
         </div>
+        </div>
+        <div class="row mt-3">
         <div>
             <img src="<?php echo $row['foto_agenda'] ?> " width='80' height='100'>
-            <input type="file" name="foto" id="foto" accept="image/*" value="<?php $row['foto_agenda'] ?>">
+            <input type="file" name="foto" id="foto" accept="image/*" value="<?php $row['foto_agenda'] ?>" class="form-control">
         </div>
+        </div>
+        <div class="row mt-3">
         <label for="data_cadastro">Data do Cadastro</label>
-        <input type="text" name="data_cadastro" value="<?php echo $row['dt_cadastro']?>" readonly="true">
+        <input type="text" name="data_cadastro" value="<?php echo $row['dt_cadastro']?>" readonly="true" class="form-control"><br>
+        </div>
+        
 
-        <input type="submit" value="Salvar">
+        <input type="submit" value="Salvar" class="btn btn-dark mt-3">
 
     </form>
+    <footer class="footer bg-dark text-white text-center">
+      <div class="container py-3">
+        <span>© 2023 Meu Website. Todos os direitos reservados.</span>
+      </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- listar_agenda.php
+++ listar_agenda.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+
+    <title>Consulta de Agendas</title>
 </head>
 <body>
     <?php
@@ -15,53 +17,83 @@
         /*mysqli_fetch_array retorna apenas uma kinha dos registros */
         $row = mysqli_fetch_array($result);
     ?>
-    <div class="">
-    <h1>Consulta de Agendas:</h1>
-    <table align="center" border="1" width="700">
-        <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Foto</th>
-            <th>Apelido</th>
-            <th>Endereço</th>
-            <th>Bairro</th>
-            <th>Cidade</th>
-            <th>Estado</th>
-            <th>Telefone</th>
-            <th>Celular</th>
-            <th>E-mail</th>
-            <th>Data de Cadastro</th>
-            <th>Alterar</th>
-            <th>Deletar</th>
-        </tr>
-        <?php
-            do{
-                echo "<tr>";
-                echo "<td>".$row['id_agenda']."</td>";
-                echo "<td>".$row['nome']."</td>";
-                if($row['foto_agenda'] == ""){
-                    echo "<td></td>";
-                }else{
-                    echo "<td> <img src='".$row['foto_agenda'] ."' width='80' height='100'/></td>";
-                }
-                echo "<td>".$row['apelido']."</td>";
-                echo "<td>".$row['endereco']."</td>";
-                echo "<td>".$row['bairro']."</td>";
-                echo "<td>".$row['cidade']."</td>";
-                echo "<td>".$row['estado']."</td>";
-                echo "<td>".$row['telefone']."</td>";
-                echo "<td>".$row['celular']."</td>";
-                echo "<td>".$row['email']."</td>";
-                echo "<td>".$row['dt_cadastro']."</td>";
-                echo "<td> <a href='altera_agenda.php?id_agenda=".$row["id_agenda"]."'>Alterar</a> </td>";
-                echo "<td><a href='excluir_agenda.php?id_agenda=".$row["id_agenda"]."'>Deletar</a></td>";
-                echo"</tr>";
-            }while($row = mysqli_fetch_array($result))
-            
-        ?>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container">
+            <a class="navbar-brand" href="#">Meu Site</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+              <li class="nav-item">
+                  <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="cadastro_agenda.html">Cadastrar</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="listar_agenda.php">Listar</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+    <div class="container">
+        <h1>Consulta de Agendas:</h1>
+        <div class="table-responsive table-striped">
+            <table class="table">
+                <tr class="thead-dark">
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Foto</th>
+                    <th>Apelido</th>
+                    <th>Endereço</th>
+                    <th>Bairro</th>
+                    <th>Cidade</th>
+                    <th>Estado</th>
+                    <th>Telefone</th>
+                    <th>Celular</th>
+                    <th>E-mail</th>
+                    <th>Data de Cadastro</th>
+                    <th>Alterar</th>
+                    <th>Deletar</th>
+                </tr>
+                <?php
+                    do{
+                        echo "<tr>";
+                        echo "<td>".$row['id_agenda']."</td>";
+                        echo "<td>".$row['nome']."</td>";
+                        if($row['foto_agenda'] == ""){
+                            echo "<td></td>";
+                        }else{
+                            echo "<td> <img src='".$row['foto_agenda'] ."' width='80' height='100'/></td>";
+                        }
+                        echo "<td>".$row['apelido']."</td>";
+                        echo "<td>".$row['endereco']."</td>";
+                        echo "<td>".$row['bairro']."</td>";
+                        echo "<td>".$row['cidade']."</td>";
+                        echo "<td>".$row['estado']."</td>";
+                        echo "<td>".$row['telefone']."</td>";
+                        echo "<td>".$row['celular']."</td>";
+                        echo "<td>".$row['email']."</td>";
+                        echo "<td>".$row['dt_cadastro']."</td>";
+                        echo "<td> <a href='altera_agenda.php?id_agenda=".$row["id_agenda"]."'>Alterar</a> </td>";
+                        echo "<td><a href='excluir_agenda.php?id_agenda=".$row["id_agenda"]."'>Deletar</a></td>";
+                        echo"</tr>";
+                    }while($row = mysqli_fetch_array($result))
+
+                ?>
+            </table>
+        </div>
         <a href="index.php">Voltar</a>
-    </table>
     </div>
     
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
